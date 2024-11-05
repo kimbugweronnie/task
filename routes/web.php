ccbdf7b178471df98684ec421814ffe25660c59e
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2024-11-05 13:36:41 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\HomeController;
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+
+Auth::routes();
+Route::middleware(['auth'])->group(function () {
+    Route::resource('task',TaskController::class);
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+});
+
